f53b9f4e5252d96c14700415fbff39219b58f63d
arturozx <arturozx@gmail.com>
2015-06-12 14:48:12 -0500

    First failed

---
+++ Droid/obj/Debug/android/src/md5d65911591a3820037075a44be11d549c/MainActivity.java
@@ -0,0 +1,46 @@
+package md5d65911591a3820037075a44be11d549c;
+
+
+public class MainActivity
+	extends md5530bd51e982e6e7b340b73e88efe666e.FormsApplicationActivity
+	implements
+		mono.android.IGCUserPeer
+{
+	static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("SignaturePadTest.Droid.MainActivity, SignaturePadTest.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("SignaturePadTest.Droid.MainActivity, SignaturePadTest.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
